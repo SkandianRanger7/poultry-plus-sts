46f0cef097ad2a48b568d983a8209fb7132af655
SkandianRanger7 <thespencerrobinson@gmail.com>
2025-01-04 18:27:36 -0500

    Buffed egg bandit to do damage for each egg drawn

--- src/main/java/poultryplus/cards/EggBandit.java
+++ src/main/java/poultryplus/cards/EggBandit.java
@@ -1,11 +1,13 @@
 package poultryplus.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import poultryplus.character.MyCharacter;
 import poultryplus.util.CardStats;
@@ -23,6 +25,7 @@ public class EggBandit extends BaseCard {
     );
     private static final int DAMAGE = 6;
     private static final int UPG_DAMAGE = 3;
+    private static final int EXT_DMG = 4;
     private static final int DRAW = 4;
 
     public EggBandit() {
@@ -30,11 +33,17 @@ public class EggBandit extends BaseCard {
 
         setDamage(DAMAGE, UPG_DAMAGE);
         setMagic(DRAW);
+        setCustomVar("extraDmg", VariableType.DAMAGE, EXT_DMG);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
         addToBot(new DrawCardAction(this.magicNumber, new EggBanditFollowUpAction()));
+        for (AbstractCard c : DrawCardAction.drawnCards) {
+            if (c.cardID.toLowerCase().contains("egg")) {
+                addToBot(new DamageAction(m, new DamageInfo(p, customVar("extraDmg"), DamageInfo.DamageType.NORMAL)));
+            }
+        }
     }
 }
